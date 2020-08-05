75b5f12505b8fed8c8d8c1e43c9b5a90a383c632
Taihua Rubin <TaihuaRubin@gmail.com>
2020-08-05 12:06:48 -0400

    made some model changes during code review

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -1,45 +1,45 @@
-const Sequelize = require('sequelize')
-const db = require('../db');
+const Sequelize = require("sequelize");
+const db = require("../db");
 
-const Product = db.define('product', {
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        }
+const Product = db.define("product", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
     },
-    category: {
-        type: Sequelize.STRING
+  },
+  category: {
+    type: Sequelize.STRING,
+  },
+  imageUrl: {
+    type: Sequelize.TEXT,
+    defaultValue:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6D09dkYwqpKaGOF0-kA2ytnLGQW7Iaotl9A&usqp=CAU",
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 0,
     },
-    imageUrl: {
-        type: Sequelize.STRING,
-        defaultValue: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6D09dkYwqpKaGOF0-kA2ytnLGQW7Iaotl9A&usqp=CAU'
+  },
+  description: {
+    type: Sequelize.TEXT,
+  },
+  quantities: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 0,
     },
-    price: {
-        type: Sequelize.DECIMAL,
-        validate: {
-            min: 0
-        }
-    },
-    description: {
-        type: Sequelize.TEXT,
-    },
-    quantities: {
-        type: Sequelize.INTEGER,
-        validate: {
-            min: 0
-        }
-    }
-    
-})
+  },
+});
 
 module.exports = Product;
 
 /**
  * instanceMethods
+ //remember to creat a calculator/convertor for the price since we made the type integer
  */
-
 
 /**
  * classMethods
