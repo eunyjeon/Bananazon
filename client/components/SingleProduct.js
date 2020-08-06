1aceed74deaa011a392fbb895ec3efe67dc2ff6f
Mona Zheng <62810761+catmemberMona@users.noreply.github.com>
2020-08-06 12:29:04 -0400

    adding to SingleProduct Component

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -3,10 +3,30 @@ import { connect } from 'react-redux';
 import {getSingleProductThunk} from '../store/singleProduct'
 
 export class SingleProduct extends Component {
+    constructor(props){
+        super(props)
+        console.log(props)
+    }
+    componentDidMount(){
+        console.log('THIS IS COMPOENENTDIDMOUNT')
+        this.props.getSingleProduct(this.props.match.params.id);
+    }
 
     render(){
+        console.log('aaaaaaaaaaaahhhhhhhhhhhhhhhhhhhh')
+        console.log('WHAT IS THIS PROPS', this.props.product)
+        const name = this.props.product.name;
+        console.log(product)
         return (
-            <div> Singles
+            <div>
+                <div>
+                    {name}
+                    {/* <img src={this.product.imageUrl} /> */}
+
+                </div> 
+                <div>
+
+                </div>
             </div>
         )
     }
