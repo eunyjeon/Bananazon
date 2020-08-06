f6fa908500991e2150c08f500c5c453496f0c68a
Ashlee Pitock <ashlee.pitock@gmail.com>
2020-08-06 12:48:02 -0400

    made progress on singleProduct not connecting to the redux store

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,41 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {getSingleProductThunk} from '../store/singleProduct'
+import { getSingleProductThunk } from '../store/singleProduct';
 
 export class SingleProduct extends Component {
-    constructor(props){
-        super(props)
-        console.log(props)
-    }
-    componentDidMount(){
-        console.log('THIS IS COMPOENENTDIDMOUNT')
-        this.props.getSingleProduct(this.props.match.params.id);
-    }
+  constructor(props) {
+    super(props);
+    console.log(props);
+  }
+  componentDidMount() {
+    this.props.getSingleProduct(this.props.match.params.id);
+  }
 
-    render(){
-        console.log('aaaaaaaaaaaahhhhhhhhhhhhhhhhhhhh')
-        console.log('WHAT IS THIS PROPS', this.props.product)
-        const name = this.props.product.name;
-        console.log(product)
-        return (
-            <div>
-                <div>
-                    {name}
-                    {/* <img src={this.product.imageUrl} /> */}
-
-                </div> 
-                <div>
-
-                </div>
-            </div>
-        )
-    }
+  render() {
+    console.log('WHAT IS THIS PROPS', this.props);
+    // const name = this.props.product.name;
+    return (
+      <div>
+        <div>
+          hello
+          {/* <img src={this.product.imageUrl} /> */}
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
-    product: state.product
-})
+  product: state.product,
+});
 const mapDispatchToProps = (dispatch) => ({
-    getSingleProduct: (id) => dispatch(getSingleProductThunk(id))
-})
-export default connect(mapStateToProps, mapDispatchToProps)(SingleProduct)
+  getSingleProduct: (id) => dispatch(getSingleProductThunk(id)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(SingleProduct);
--- client/store/index.js
+++ client/store/index.js
@@ -3,11 +3,11 @@ import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import user from './user';
-import product from './singleProduct';
+import singleProductReducer from './singleProduct';
 
 const reducer = combineReducers({
   user,
-  product,
+  product: singleProductReducer,
 });
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
