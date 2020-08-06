43e48ff76a9f67357769dd06adc3e98cdef88b66
Ashlee Pitock <ashlee.pitock@gmail.com>
2020-08-06 17:22:40 -0400

    conversion updated

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -1,56 +1,44 @@
-const Sequelize = require("sequelize");
-const db = require("../db");
+const Sequelize = require('sequelize');
+const db = require('../db');
 
-const Product = db.define("product", {
+const Product = db.define('product', {
   name: {
     type: Sequelize.STRING,
     allowNull: false,
     validate: {
-      notEmpty: true
-    }
+      notEmpty: true,
+    },
   },
   category: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
   },
   imageUrl: {
     type: Sequelize.TEXT,
     defaultValue:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6D09dkYwqpKaGOF0-kA2ytnLGQW7Iaotl9A&usqp=CAU"
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6D09dkYwqpKaGOF0-kA2ytnLGQW7Iaotl9A&usqp=CAU',
   },
   price: {
     type: Sequelize.INTEGER,
     allowNull: false,
     validate: {
       min: 0,
-      isInt: true
-    }
-    //not completly confident this setter will work
-    // set: function () {
-    //   return new Intl.NumberFormat('en-US', {
-    //     style: 'currency',
-    //     currency: 'USD',
-    //   });
-    // },
+      isInt: true,
+    },
+    get() {
+      const rawValue = this.getDataValue('price');
+      return rawValue ? rawValue / 100 : null;
+    },
   },
   description: {
-    type: Sequelize.TEXT
+    type: Sequelize.TEXT,
   },
   quantities: {
     type: Sequelize.INTEGER,
     defaultValue: 0,
     validate: {
-      min: 0
-    }
-  }
+      min: 0,
+    },
+  },
 });
 
 module.exports = Product;
-
-/**
- * instanceMethods
- //remember to creat a calculator/convertor for the price since we made the type integer
- */
-
-/**
- * classMethods
- */
