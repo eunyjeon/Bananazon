b2c5fc895731c2d0298157ff34dbe2603ed244a8
Mona Zheng <62810761+catmemberMona@users.noreply.github.com>
2020-08-07 14:56:02 -0400

    created order.js file for cart backend

---
+++ server/api/order.js
@@ -0,0 +1,11 @@
+const orderRouter = require("express").Router();
+
+orderRouter.post("/", async (req,res,next) => {
+    try {
+        const order 
+    } catch (error) {
+        next(error);
+    };
+});
+
+module.exports = orderRouter;
