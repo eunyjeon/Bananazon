9b51fb3f09ef05736954cf493536944b3fc8efcb
Mona Zheng <62810761+catmemberMona@users.noreply.github.com>
2020-08-08 10:34:22 -0400

    working on get api request to send all order info to front end

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,6 +1,17 @@
 const orderRouter = require("express").Router();
 const { Order } = require("../db/model")
 
+orderRouter.get("/:orderId", async (req,res,next) => {
+    try {
+        const orderId = req.params.orderId;
+        const order = await Order.findByPk(orderId, {
+            include: {}
+        })
+    } catch (error) {
+        next(error);
+    };
+});
+
 // if (user's) order.isPaid is set to true && user clicks on addTo Cart
 orderRouter.post("/", async (req,res,next) => {
     try {
