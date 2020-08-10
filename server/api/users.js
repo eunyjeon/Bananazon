26f6b892cff34cef2999b89fb5556c7eb38d8833
Ashlee Pitock <ashlee.pitock@gmail.com>
2020-08-10 15:11:57 -0400

    added isAdmin util function and made a route to test

--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../db/models');
+const { isAdmin } = require('../../utility/utilityFunc');
 module.exports = router;
 
 router.get('/', async (req, res, next) => {
@@ -8,10 +9,31 @@ router.get('/', async (req, res, next) => {
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'email']
+      attributes: ['id', 'email'],
     });
     res.json(users);
   } catch (err) {
     next(err);
+  }
+});
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId);
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+router.delete('/:userId', isAdmin, async (req, res, next) => {
+  const userId = +req.params.userId;
+  try {
+    await User.destroy({
+      where: {
+        id: userId,
+      },
+    });
+    res.json(userId);
+  } catch (error) {
+    next(error);
   }
 });
--- utility/utilityFunc.js
+++ utility/utilityFunc.js
@@ -1,12 +1,19 @@
 // called when a user clicks checkout btn
 export const deleteLocalCart = () => {
-  localStorage.removeItem("orderId")
-}
+  localStorage.removeItem('orderId');
+};
 
 // called when a user clicks addToCart btn from SingleProduct and AllProduct to create {"orderId": orderId} in localStorage
 export const createLocalCart = (orderId) => {
-  if (!("orderId" in localStorage)) {
-    localStorage.setItem("orderId", orderId)
+  if (!('orderId' in localStorage)) {
+    localStorage.setItem('orderId', orderId);
   }
-}
-
+};
+//adding a checker to see if a User is admin or not
+export const isAdmin = (req, res, next) => {
+  if (!req.user || !req.user.isAdmin) {
+    const error = new Error(`Nice try, you're not an Admin!`);
+    return next(error);
+  }
+  next();
+};
